f0a59b4c698260dbc121307193dc04b59b7147b8
jerhadf <jhadfield129@gmail.com>
2023-12-09 03:01:50 -0700

    Updated extension

---
+++ hume_raycast/hume-ai/src/ask-hume.tsx
@@ -0,0 +1,13 @@
+ // api request curl
+//  curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer <your-api-key>"
+//   -d '{"text":"Mary had a little lamb,"}' https://api.hume.ai/endpoint
+
+
+ // This is a comment in TypeScript
+//  import { Action, ActionPanel, List, showToast, Toast } from "@raycast/api";
+
+//  import { RaycastExtensionItem } from '@raycast/api';
+
+
+
+
---
+++ hume_raycast/hume-ai/src/index.tsx
@@ -0,0 +1,47 @@
+// entry point
+
+// get the content of their clipboard currently - use a form?
+// https://developers.raycast.com/api-reference/clipboard
+
+import { Action, ActionPanel, Form } from "@raycast/api";
+
+export default function Command() {
+  return (
+    <Form
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm title="Submit Answer" onSubmit={(values) => console.log(values)} />
+        </ActionPanel>
+      }
+    >
+      <Form.Checkbox id="answer" label="Are you happy?" defaultValue={true} />
+    </Form>
+  );
+}
+
+
+
+// paste results / output to clipboard
+import { Clipboard } from "@raycast/api";
+
+export default async function Command() {
+  // copy some text
+  await Clipboard.copy("https://raycast.com");
+
+  const textContent: Clipboard.Content = {
+    text: "https://raycast.com",
+  };
+  await Clipboard.copy(textContent);
+
+  // copy a file
+  const file = "/path/to/file.pdf";
+  try {
+    const fileContent: Clipboard.Content = { file };
+    await Clipboard.copy(fileContent);
+  } catch (error) {
+    console.log(`Could not copy file '${file}'. Reason: ${error}`);
+  }
+
+  // copy confidential data
+  await Clipboard.copy("my-secret-password", { concealed: true });
+}
